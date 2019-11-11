ad31e6958165fffc083c0c6b701db7deca57f7d9
Jeremy Buttice <jeremy@roamdapper.fr>
2019-11-11 16:13:04 +0100

    chore: first

---
+++ main.py
@@ -0,0 +1,52 @@
+#!/bin/python
+
+import json
+import requests
+import os
+import argparse
+from slack import Slack, SlackMessage
+from radarr import RadarrApi
+from tmdb import TmdbApi
+
+def _argparse():
+    parser = argparse.ArgumentParser(
+        description='Radarr Custom Script : perform Slack rich notification'
+    )
+    parser.add_argument(
+        '--webhook-url', '-wu',
+        help='Slack webhook url'
+    )
+    parser.add_argument(
+        '--radarr-url', '-re',
+        help='Radarr API endpoint : https://xxxx/api'
+    )
+    parser.add_argument(
+        '--radarr-key', '-rk',
+        help='Radarr API key, find it on Radarr > Settings > General'
+    )
+    parser.add_argument(
+        '--tmdb-key', '-tk',
+        help='TMDB API Key, register app on tmdb to obtain API Key'
+    )
+    args = parser.parse_args()
+    return args
+
+
+args = _argparse()
+
+radarr = RadarrApi(args.radarr_url, args.radarr_key)
+radarr.unmonitorMovieIfNeeded(os.environ.get("radarr_movie_id"), os.environ.get("radarr_eventtype"))
+radarr.loadData(os.environ.get("radarr_download_id"))
+link = "https://www.themoviedb.org/movie/"+os.environ.get("radarr_movie_tmdbid", "")
+
+tmdb = TmdbApi(args.tmdb_key)
+tmdb.loadMovieData(radarr.tmdbId)
+tmdb.downloadMovieProductionImage()
+
+message = SlackMessage(args.webhook_url)
+message.package("*"+ os.environ.get("radarr_movie_title", "") +"* ("+ radarr.year +") ["+ os.environ.get("radarr_moviefile_quality", "") +"]")
+message.constructor("`" +radarr.indexer+"` _"+os.environ.get("radarr_moviefile_releasegroup", "")+"_ ("+radarr.sizeOnDisk+")")
+message.link(link)
+message.save()
+
+message.notify()
---
+++ radarr.py
@@ -0,0 +1,74 @@
+import requests
+import json
+
+class RadarrApi:
+    baseUrl = None
+    apiKey = None
+
+    indexer = ""
+    year = ""
+    tmdbId = None
+    sizeOnDisk = ""
+
+    def __init__(self, baseUrl, apiKey):
+        self.baseUrl = baseUrl
+        self.apiKey = apiKey
+
+    def loadData(self, downloadId):
+        if self.apiKey is None or self.baseUrl is None or downloadId is None:
+            return
+
+        payload = {"apikey": self.apiKey, "page": 1, "sortKey": "date", "sortDir": "desc", "pageSize": 20}
+        response = requests.get(self.baseUrl + "/history", params=payload)
+        if response.status_code != 200:
+            raise ValueError(
+                'Request returned an error %s, the response is:\n%s'
+                % (response.status_code, response.text)
+            )
+        data = dict(json.loads(response.text))
+        for record in data["records"]:
+            record = dict(record)
+            recordDownloadId = record.get("downloadId")
+            indexer = record.get("data", {}).get("indexer")
+            if recordDownloadId == downloadId and indexer is not None:
+                movie = record.get("movie", {})
+                self.year = str(movie.get("year", ""))
+                self.indexer = indexer
+                self.tmdbId = str(movie.get("tmdbId", ""))
+                size = int(record.get("data", {}).get("size", "0"))
+                self.sizeOnDisk = self.sizeof_fmt(size)
+                return
+        return
+
+    def getMovie(self, movieId):
+        payload = {"apikey": self.apiKey}
+        response = requests.get(self.baseUrl + "/movie/" + movieId, params=payload)
+        if response.status_code != 200:
+            raise ValueError(
+                'Request returned an error %s, the response is:\n%s'
+                % (response.status_code, response.text)
+            )
+        return json.loads(response.text)
+
+    def unmonitorMovie(self, movie):
+        movie["monitored"] = False
+        headers = {'Content-type': 'application/json', 'X-Api-Key': self.apiKey}
+        response = requests.put(self.baseUrl + "/movie/" + str(movie["id"]), data=json.dumps(movie), headers=headers)
+        if response.status_code != 202:
+            raise ValueError(
+                'Request returned an error %s, the response is:\n%s'
+                % (response.status_code, response.text)
+            )
+
+    def unmonitorMovieIfNeeded(self, movieId, event):
+        if event == "Download":
+            movie = self.getMovie(movieId)
+            if movie.get("monitored", False):
+                self.unmonitorMovie(movie)
+
+    def sizeof_fmt(self, num, suffix='o'):
+        for unit in ['','K','M','G','T','P','E','Z']:
+            if abs(num) < 1024.0:
+                return "%3.1f%s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.1f%s%s" % (num, 'Y', suffix)
